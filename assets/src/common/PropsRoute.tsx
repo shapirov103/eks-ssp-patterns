f87215a958f6a1b0a55f50860c413a6610508c11
Mikhail Shapirov <shapirov@amazon.com>
2021-05-21 10:46:15 -0400

    cleaned up template files that are not needed

--- assets/src/common/PropsRoute.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Route } from "react-router";
-
-//@ts-ignore
-export default ({ component: C, props: cProps, ...rest }) =>
-  <Route {...rest} render={props => <C {...props} {...cProps} />} />;
--- assets/src/modules/notFound/NotFound.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./notFound.css";
-
-export default () =>
-  <div className="not-found">
-    <h3>Sorry, page not found!</h3>
-  </div>;
