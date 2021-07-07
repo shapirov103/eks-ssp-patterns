a9fc0286bbcf8412cc169a65ff644ecbede84eef
shapirov103 <shapirov@amazon.com>
2021-07-07 01:28:13 -0400

    wip ingress stack

--- lib/nginx-ingress-construct/index.ts
+++ lib/nginx-ingress-construct/index.ts
@@ -12,20 +12,63 @@ import * as team from '../teams'
 import { valueFromContext } from '@shapirov/cdk-eks-blueprint/dist/utils/context-utils';
 import { Stack } from '@aws-cdk/core';
 
-export default class NginxIngressConstruct extends cdk.Stack {
+const accountID = process.env.CDK_DEFAULT_ACCOUNT!;
+
+
+export default class NginxIngressConstruct extends ssp.EksBlueprint {
+
     constructor(scope: cdk.Construct, id: string) {
-        super(scope, id);
 
-        // Setup platform team
-        const accountID = process.env.CDK_DEFAULT_ACCOUNT!;
+        // Teams for the cluster.
+        const teams: Array<ssp.Team> = [
+            new team.TeamPlatform(accountID),
+            new team.TeamTroiSetup,
+            new team.TeamRikerSetup,
+            new team.TeamBurnhamSetup(scope)
+        ];
+
+        const subZoneName = valueFromContext(scope, "dev.subzone", "dev.some.example.com");
+        // AddOns for the cluster.
+        const addOns: Array<ssp.ClusterAddOn> = [
+            new ssp.AwsLoadBalancerControllerAddOn,
+            new ssp.addons.ExternalDnsAddon({
+                hostedZones: [route53.HostedZone.fromLookup(scope, "dns-sub-zone", { domainName: subZoneName })]
+            }),
+            new ssp.NginxAddOn({ internetFacing: true, backendProtocol: "tcp", externaDnsHostname: subZoneName }),
+            new ssp.ArgoCDAddOn,
+            new ssp.CalicoAddOn,
+            new ssp.MetricsServerAddOn,
+            new ssp.ClusterAutoScalerAddOn,
+            new ssp.ContainerInsightsAddOn,
+        ];
+
+        const stackID = `${id}-blueprint`;
+        super(scope, { id: stackID, addOns, teams }, {
+            env: {
+                region: 'us-west-1',
+            },
+        });
+    }
+}
+
+/**
+ * Stack creates Route 53 configuration.
+ */
+export class Nginx extends cdk.Stack {
+
+    public subZone: route53.IHostedZone;
+
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
         const parentDnsAccountId = this.node.tryGetContext("parent.dns.account")!;
-        const subZoneName = valueFromContext(this, "dev.subzone", "*.dev.some.example.com");
+        const subZoneName = valueFromContext(this, "dev.subzone", "dev.some.example.com");
 
-        const platformTeam = new team.TeamPlatform(accountID);
-
-        const subZone = new route53.PublicHostedZone(this, 'SubZone', {
+        this.subZone = new route53.PublicHostedZone(this, 'SubZone', {
             zoneName: subZoneName
         });
+
+        this.exportValue(this.subZone.hostedZoneArn);
 
         const parentHostedZoneName = valueFromContext(this, "parent.hosted-zone.name", "some.example.com");
 
@@ -46,35 +89,9 @@ export default class NginxIngressConstruct extends cdk.Stack {
 
         // create the record
         new route53.CrossAccountZoneDelegationRecord(this, 'delegate', {
-            delegatedZone: subZone,
+            delegatedZone: this.subZone,
             parentHostedZoneName: parentHostedZoneName, // or you can use parentHostedZoneId
             delegationRole,
         });
-
-        // Teams for the cluster.
-        const teams: Array<ssp.Team> = [
-            platformTeam,
-            new team.TeamTroiSetup,
-            new team.TeamRikerSetup,
-            new team.TeamBurnhamSetup(scope)
-        ];
-
-        // AddOns for the cluster.
-        const addOns: Array<ssp.ClusterAddOn> = [
-            new ssp.NginxAddOn({ internetFacing: true, backendProtocol: "tcp", externaDnsHostname: subZoneName }),
-            new ssp.ArgoCDAddOn,
-            new ssp.CalicoAddOn,
-            new ssp.MetricsServerAddOn,
-            new ssp.ClusterAutoScalerAddOn,
-            new ssp.ContainerInsightsAddOn,
-        ];
-
-        const stackID = `${id}-blueprint`
-        new ssp.EksBlueprint(scope, { id: stackID, addOns, teams }, {
-            env: {
-                region: 'us-east-2',
-            },
-        });
     }
 }
-
